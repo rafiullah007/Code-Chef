accf6c0db7d38ce98988b2595406e60724ac3075
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-03-14 22:30:25 +0600

    Add files via upload

---
+++ Summer Time.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	if(n>35)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+
+}
+
